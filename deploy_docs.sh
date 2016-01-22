db56abe70985b63267d19f2b2336df5a0ce8707c
Joseph Post <jp@jsphpst.com>
2016-01-22 12:53:15 -0600

    don't remove files that don't exist

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -15,12 +15,11 @@ cp -r target/doc/ $TEMP_DIRECTORY || exit 1
 echo "Checking out gh-pages branch"  
 git checkout -B gh-pages || exit 1
 
-echo "Removing old static content"  
+echo "Removing unneeded files"  
 git rm -rf . || exit 1
 
 echo "Copying newly generated static content"
 cp -r $TEMP_DIRECTORY/* .       || exit 1
-cp $TEMP_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis-CI"         || exit 1
